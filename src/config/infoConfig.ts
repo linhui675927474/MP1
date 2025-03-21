adfb935cb9b52fa7f05c2fe6ed2ebef596003543
林惠 <15076831+linhui675927474@user.noreply.gitee.com>
2025-03-21 17:20:40 +0800

    2025-03-21 update name headline and email

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -3,11 +3,11 @@ export * from './education'
 export * from './career'
 
 // personal info
-export const name = 'Derek Hu'
-export const headline = 'Teacher at Shanghai Soong Ching Ling School.'
+export const name = 'Hui Lin'
+export const headline = 'A Biomedical Engineering.'
 export const introduction =
   "Coucou Les Amis! My name is Derek Hu. I'm from Hubei and an alumnus of BUPT, UChicago, and Tsinghua. I’ve been teaching Computer Science at SCLS since 2020."
-export const email = 'tong.hu@scls-sh.org'
+export const email = 'linhui675927474@zju.edu.cn'
 export const githubUsername = 'derekhut'
 
 // about page
